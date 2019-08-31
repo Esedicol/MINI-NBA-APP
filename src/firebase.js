4a6d60dc2ed96b17cd4283f286b7379831b02444
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2019-08-31 09:31:11 +0100

    Update firebase.js
    updated the firebase config details

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 
+/*
 var config = {
     apiKey: "AIzaSyD3tV72d4iKQdjPOdFDXeIsyKl5EmWxwF4",
     authDomain: "mini-gsw.firebaseapp.com",
@@ -8,6 +9,17 @@ var config = {
     projectId: "mini-gsw",
     storageBucket: "mini-gsw.appspot.com",
     messagingSenderId: "981223853683"
+};
+*/
+
+var config = {
+    apiKey: "AIzaSyB2yLJMxVgQXqHilG123lsbCgSS7ugx8jg",
+    authDomain: "mini-nba-app-e0356.firebaseapp.com",
+    databaseURL: "https://mini-nba-app-e0356.firebaseio.com",
+    projectId: "mini-nba-app-e0356",
+    storageBucket: "mini-nba-app-e0356.appspot.com",
+    messagingSenderId: "283513184036",
+    appId: "1:283513184036:web:efb96562f680184f"
 };
 
 firebase.initializeApp(config);
